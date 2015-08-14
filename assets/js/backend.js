1c8d3839263c8dfc149a62039475798c9c2dde8b
nabeel <nabeel@qburst.com>
2015-08-14 15:19:08 +0530

    added signup popup

--- assets/js/backend.js
+++ assets/js/backend.js
@@ -19,26 +19,50 @@ app.use(function (req, res, next) {
     // Pass to next layer of middleware
     next();
 });
+
+// app.use(express.bodyParser());
+var LocalStorage = require('node-localstorage').LocalStorage,
+		localStorage = new LocalStorage('./scratch');
+
 var backendcallback = function(){
-	var userData = {}
-	app.get('/demo', function(req, res){
-		console.log(req.query.username);
-		console.log(req.query.password);
+	var userData;
+	app.post('/login', function(req, res){
+		userData = JSON.parse(localStorage.getItem('signup-data'));
 		var response = {
 			status  : 200,
-			success : ''
+			success : 'loggedin-succussfully'
 		}
-		var users = [
-						{"username": "nabeel", "password":"nabeel"},
-						{"username": "nikhil", "password":"nikhil"}
-					];
-		if(req.query.username == "nabeel" && req.query.password == "nabeel"){
-			response.success = "success";
-		}else{
-			response.success = "failure";
+
+		console.log(userData);
+		// if(req.query.username == "nabeel" && req.query.password == "nabeel"){
+		// 	response.success = "success";
+		// }else{
+		// 	response.success = "failure";
+		// }
+		res.end(JSON.stringify(response));
+	});
+
+	app.post('/signup', function(req, res){
+		// var userData = [];
+		var data = {
+			"name" : req.query.name,
+			"phone" : req.query.phone,
+			"username" : req.query.username,
+			"password" : req.query.password
 		}
+		console.log("data--->",data);
+		// console.log(req);
+		var response = {
+			status  : 200,
+			success : 'Signup is successfull.'
+		}
+		// userData.push(JSON.stringify(data));
+		data = JSON.stringify(data);
+   		localStorage.setItem('signup-data', data);
+		console.log("local-storage-data",localStorage.getItem('signup-data'));
 		res.end(JSON.stringify(response));
 	});
 }
 backendcallback();
 app.listen(3000);
+
--- assets/js/index.js
+++ assets/js/index.js
@@ -3,6 +3,17 @@ $(document).ready(function() {
 		e.preventDefault();
 		login();
 	});
+	$(".close").on("click",function(){
+		$(".popup,.bk-overlay").fadeOut();
+	});
+	$("#sign-up").on("click",function(){
+		$(".sign-up-popup,.bk-overlay").fadeIn();
+	});	
+
+	$("#signup-submit").on("click" ,function(e){
+		e.preventDefault();
+		signup();
+	});
 });
 
 function login(){
@@ -13,16 +24,49 @@ function login(){
 			"password" : password
 		};
 	 $.ajax({
-        url: 'http://10.1.4.87:3000/demo',
-        type: 'GET',
+        url: 'http://10.1.4.87:3000/login',
+        type: 'POST',
+        dataType:"json",
+        data: data,
+        error: function(response) {
+            // $(".popup,.bk-overlay").fadeIn();
+            // $("span.message").text(response.success);
+            // callback.failure(XMLHttpRequest, textStatus);
+        },
+        success: function(response) {
+            // $(".popup,.bk-overlay").fadeIn();
+            // $("span.message").text(response.success);
+        },
+        complete: function(response) {
+            // onEndAjaxRequest();
+        }
+    });
+}
+
+function signup(){
+	var username = $("#username-signup").val(),
+		password = $("#password-signup").val(),
+		name = $("#name").val(),
+		phone = $("#phone").val(),
+		data = {
+			"name" : name,
+			"phone" : phone, 
+			"username" : username,
+			"password" : password
+		};
+	 $.ajax({
+        url: 'http://10.1.4.87:3000/signup',
+        type: 'POST',
         dataType:"json",
         data: data,
         error: function(response) {
             // callback.failure(XMLHttpRequest, textStatus);
+            alert("error");
         },
         success: function(response) {
-            // callback.success(data, textStatus, flag);
-            console.log(response);
+            // $(".popup,.bk-overlay").fadeIn();
+            // $("span.message").text(response.success);
+            alert(response);
         },
         complete: function(response) {
             // onEndAjaxRequest();
